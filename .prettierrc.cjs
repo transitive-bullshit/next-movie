973d0b20f97aee8fc99055a331b68de49e7a1949
Travis Fischer <fisch0920@gmail.com>
2022-11-17 22:58:51 -0600

    chore: add comments to config files

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,5 +1,4 @@
 module.exports = {
-  // plugins: [require('@trivago/prettier-plugin-sort-imports')],
   singleQuote: true,
   jsxSingleQuote: true,
   semi: false,
@@ -9,13 +8,4 @@ module.exports = {
   bracketSameLine: false,
   arrowParens: 'always',
   trailingComma: 'none'
-  // importOrder: [
-  //   '^(react/(.*)$)|^(react$)|^(next/(.*)$)|^(next$)',
-  //   '<THIRD_PARTY_MODULES>',
-  //   '^(@/lib/(.*)$)|^(@/components/(.*)$)|^(@/icons/(.*)$)',
-  //   '^[./]'
-  // ],
-  // importOrderSeparation: true,
-  // importOrderSortSpecifiers: true,
-  // importOrderGroupNamespaceSpecifiers: true
 }
