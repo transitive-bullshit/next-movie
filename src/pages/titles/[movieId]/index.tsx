e821eb48cf0c5149babb50a4b80e7384a550949a
Travis Fischer <fisch0920@gmail.com>
2022-11-24 18:23:47 -0600

    feat: add UserMovie to movie details page

--- src/pages/titles/[movieId]/index.tsx
+++ src/pages/titles/[movieId]/index.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react'
-import { InferGetStaticPropsType } from 'next'
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next'
 
 import * as config from '@/lib/config'
+import * as types from '@/types'
 import { Layout } from '@/components/Layout/Layout'
 import { Movie } from '@/components/Movie/Movie'
 import { PageHead } from '@/components/PageHead/PageHead'
 import { getMoviePathname, parseMovieId } from '@/lib/utils'
+import { getServerSession } from '@/server/auth'
 import { prisma } from '@/server/prisma'
 import { convertMovie } from '@/server/utils'
 
@@ -13,7 +15,9 @@ import styles from './styles.module.css'
 
 export default function MovieDetailPage({
   movie
-}: InferGetStaticPropsType<typeof getStaticProps>) {
+}: {
+  movie: types.MovieModel
+}) {
   return (
     <Layout>
       {movie && (
@@ -26,7 +30,7 @@ export default function MovieDetailPage({
           />
 
           <div className={styles.container}>
-            <Movie movie={movie} priority={true} />
+            <EditableMovie movie={movie} />
           </div>
         </>
       )}
@@ -34,22 +38,62 @@ export default function MovieDetailPage({
   )
 }
 
-export const getStaticProps = async ({
-  params
-}: {
-  params: { movieId: string }
-}) => {
-  const id = parseMovieId(params.movieId)
+function EditableMovie({ movie: initialMovie }: { movie: types.MovieModel }) {
+  const [movie, setMovie] = React.useState(initialMovie)
+
+  const mutateUserMovie = React.useCallback<types.MutateUserMovieFn>(
+    async (userMovie) => {
+      const { movieId, ...patch } = userMovie
+
+      const update = await fetch(`/api/titles/${movieId}/user-movie`, {
+        method: 'POST',
+        body: JSON.stringify(patch),
+        headers: {
+          'content-type': 'application/json'
+        }
+      }).then((res) => res.json())
+
+      setMovie((movie) => ({
+        ...movie,
+        userMovie: update
+      }))
+    },
+    []
+  )
+
+  return (
+    <div className={styles.container}>
+      <Movie movie={movie} priority={true} mutateUserMovie={mutateUserMovie} />
+    </div>
+  )
+}
+
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  const id = parseMovieId(context.params?.movieId as string)
   if (!id) {
     return {
       notFound: true
     }
   }
 
+  const session = await getServerSession(context.req, context.res)
+  const include: types.Prisma.MovieInclude | undefined = session?.user?.id
+    ? {
+        userMovies: {
+          where: {
+            userId: session.user.id
+          }
+        }
+      }
+    : undefined
+
   const result = await prisma.movie.findUnique({
     where: {
       id
-    }
+    },
+    include
   })
 
   if (!result) {
@@ -67,33 +111,33 @@ export const getStaticProps = async ({
   }
 }
 
-export async function getStaticPaths() {
-  if (config.isDev) {
-    return {
-      paths: [],
-      fallback: true
-    }
-  }
+// export async function getStaticPaths() {
+//   if (config.isDev) {
+//     return {
+//       paths: [],
+//       fallback: true
+//     }
+//   }
 
-  const movies = await prisma.movie.findMany({
-    select: {
-      id: true
-    },
-    orderBy: {
-      relevancyScore: 'desc'
-    },
-    // TODO
-    take: 10
-  })
+//   const movies = await prisma.movie.findMany({
+//     select: {
+//       id: true
+//     },
+//     orderBy: {
+//       relevancyScore: 'desc'
+//     },
+//     // TODO
+//     take: 10
+//   })
 
-  const paths = movies.map((movie) => ({
-    params: {
-      movieId: `${movie.id}`
-    }
-  }))
+//   const paths = movies.map((movie) => ({
+//     params: {
+//       movieId: `${movie.id}`
+//     }
+//   }))
 
-  return {
-    paths,
-    fallback: true
-  }
-}
+//   return {
+//     paths,
+//     fallback: true
+//   }
+// }
